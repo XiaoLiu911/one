7c918c8434131c560916ae089863a44ef15a7cbb
Your Name <your.email@example.com>
2014-12-31 10:56:38 +0800

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dalong::Application.config.secret_token = '077fd65b2d49bcd959ae2d993f0c10d82102fa4ebb1585ec403660cebe38857850099798938044453001187b5bd20548dd1ca02d1b06be83d2a76b15060b1156'
